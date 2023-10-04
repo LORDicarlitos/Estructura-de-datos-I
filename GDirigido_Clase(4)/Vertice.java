c2f3bffa4ec6f713fb0243d79ee179055380c529
LORDicarlitos <127449795+LORDicarlitos@users.noreply.github.com>
2023-10-03 19:32:19 -0500

    Add files via upload

---
+++ GDirigido_Clase(4)/Vertice.java
@@ -0,0 +1,50 @@
+package grafo_dirigido;
+
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+
+public class Vertice {
+    String dato;
+    ArrayList<Vertice> adyacente;
+    int ID;
+    static int cd;
+
+    public Vertice(String dato) {
+        this.dato = dato;
+        this.adyacente = new ArrayList<>();
+        this.ID = cd++;
+        
+    }
+    public void agregar_adyacente (Vertice v ){
+    this.adyacente.add(v);
+    
+    
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public ArrayList<Vertice> getAdyacente() {
+        return adyacente;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public static int getCd() {
+        return cd;
+    }
+
+    public String toString() {
+        return "Vertice{" +
+            "dato=" + dato +
+            ",adyacentes="+ adyacente.stream().map(Vertice::getDato).map(Object::toString).collect(Collectors.joining(", ")) +
+            '}';
+
+    }
+    
+}
