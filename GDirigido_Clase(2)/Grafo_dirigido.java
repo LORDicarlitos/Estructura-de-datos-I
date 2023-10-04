b6d35adfe0d341736b33c06e7a590883c045b901
LORDicarlitos <127449795+LORDicarlitos@users.noreply.github.com>
2023-10-04 07:12:02 -0500

    Update Grafo_dirigido.java

--- GDirigido_Clase(2)/Grafo_dirigido.java
+++ GDirigido_Clase(2)/Grafo_dirigido.java
@@ -1,32 +1,25 @@
-
 package grafo_dirigido;
-
 import java.util.ArrayList;
 
 public class Grafo_dirigido {
-
+     
      ArrayList<Vertice> vertices = new ArrayList();
-    int adyacencia[][];
-    
-    /**
-     * @param args the command line arguments
-     */
-    public Grafo_dirigido(){
-    this.vertices = new ArrayList<>();
-    this.adyacencia = new int [100][100];
-    
-    
-    }
-    public void agregar_vertice(Vertice v){
-    this.vertices.add(v);
-    
-    }
-    
-    public void conectar_vertice(Vertice v1, Vertice v2){
-    v1.agregar_adyacente(v2);
-    adyacencia[v1.getID()][v2.getID()]=1;
-   
-    }
+     int adyacencia[][];
+
+     //Constructor
+     public Grafo_dirigido(){
+          this.vertices = new ArrayList<>();
+          this.adyacencia = new int [100][100];
+     }
+     
+     public void agregar_vertice(Vertice v){
+          this.vertices.add(v);
+     }
+     
+     public void conectar_vertice(Vertice v1, Vertice v2){
+          v1.agregar_adyacente(v2);
+          adyacencia[v1.getID()][v2.getID()]=1;
+     }
     
     public void imprimir_grafo(){
         for (Vertice vertice : vertices) {
