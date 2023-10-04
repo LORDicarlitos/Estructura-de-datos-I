cbb76d1b9c3498828cfec25c09072ef8e7daf849
LORDicarlitos <127449795+LORDicarlitos@users.noreply.github.com>
2023-10-04 07:21:57 -0500

    Update main.java

--- GDirigido_Clase(4)/main.java
+++ GDirigido_Clase(4)/main.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package grafo_dirigido;
-
 import java.util.ArrayList;
 
-/**
- *
- * @author Autologon
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Grafo_dirigido grafo = new Grafo_dirigido();
 
