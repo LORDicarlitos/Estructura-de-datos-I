7e690569709df4fbcda6a39fa4f116624752ac89
LORDicarlitos <127449795+LORDicarlitos@users.noreply.github.com>
2023-10-04 07:24:25 -0500

    Update Grafo_dirigido.java

--- GDirigido_Clase(4)/Grafo_dirigido.java
+++ GDirigido_Clase(4)/Grafo_dirigido.java
@@ -1,53 +1,28 @@
-
 package grafo_dirigido;
-
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class Grafo_dirigido {
-
+     
      ArrayList<Vertice> vertices = new ArrayList();
-    int adyacencia[][];
-    
-    /**
-     * @param args the command line arguments
-     */
-    public Grafo_dirigido(){
-    this.vertices = new ArrayList<>();
-    this.adyacencia = new int [100][100];
-    
-    
-    }
-    public void agregar_vertice(Vertice v){
-    this.vertices.add(v);
-    
-    }
-    
-    public void conectar_vertice(Vertice v1, Vertice v2){
-    v1.agregar_adyacente(v2);
-    adyacencia[v1.getID()][v2.getID()]=1;
-   
-    }
-    
-    public void imprimir_grafo(){
-        for (Vertice vertice : vertices) {
-            System.out.println(vertice.toString());   
-        }
-        System.out.println("matriz de adyacencia");
-        
-        for (int i = 0; i < vertices.size(); i++) {
-            
-            for (int j = 0; j < vertices.size(); j++) {
-                System.out.print(adyacencia[i][j]);  
-            }
-            System.out.println("");
-  
-        }
-    
-    } 
-    
-    
-    public ArrayList<Vertice> recorridoDFS(Vertice v){
+     int adyacencia[][];
+
+     //Constructor
+     public Grafo_dirigido(){
+          this.vertices = new ArrayList<>();
+          this.adyacencia = new int [100][100];
+     }
+     
+     public void agregar_vertice(Vertice v){
+          this.vertices.add(v);
+     }
+     
+     public void conectar_vertice(Vertice v1, Vertice v2){
+          v1.agregar_adyacente(v2);
+          adyacencia[v1.getID()][v2.getID()]=1;
+     }
+     
+     public ArrayList<Vertice> recorridoDFS(Vertice v){
         boolean[] visitados = new boolean[vertices.size()];
         ArrayList<Vertice> ruta = new ArrayList();
         Stack<Vertice> pila = new Stack();
@@ -67,5 +42,5 @@ public class Grafo_dirigido {
         }
         return ruta;
     }
-    
+
 }
