e30a78796fc5293fb045290662f787e8fad8513b
LORDicarlitos <127449795+LORDicarlitos@users.noreply.github.com>
2023-10-04 07:18:43 -0500

    Update Vertice.java

--- GBFS_Clase(3)/Vertice.java
+++ GBFS_Clase(3)/Vertice.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejerciciobfs;
-
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-/**
- *
- * @author estudiante
- */
 public class Vertice {
 
     String dato;
