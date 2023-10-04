71b0731c7aae9269ebcc9487c1a3ee51d1281779
LORDicarlitos <127449795+LORDicarlitos@users.noreply.github.com>
2023-10-04 07:01:26 -0500

    Update Grafo_no_dirijido.java

--- GNODirigido_Clase(1)/Grafo_no_dirijido.java
+++ GNODirigido_Clase(1)/Grafo_no_dirijido.java
@@ -1,21 +1,18 @@
 package grafo_no_dirijido;
-
 import java.util.ArrayList;
-
 public class Grafo_no_dirijido {
 
     ArrayList<Vertice> vertices = new ArrayList();
     int adyacencia[][];
-    
+
+    //Constructor
     public Grafo_no_dirijido(){
     this.vertices = new ArrayList<>();
     this.adyacencia = new int [100][100];
-    
-    
     }
+    
     public void agregar_vertice(Vertice v){
     this.vertices.add(v);
-    
     }
     
     public void conectar_vertice(Vertice v1, Vertice v2){
@@ -26,13 +23,18 @@ public class Grafo_no_dirijido {
     }
     
     public void imprimir_grafo(){
+
+        //El método toString() se utiliza para obtener una representación en cadena (string) del objeto.
+        //La implementación predeterminada del método devuelve una cadena con el nombre de la clase y una representación hexadecimal del valor del identificador del objeto en memoria.
+        
+        //Bucle for each (funciona con colecciones o arrays)
         for (Vertice vertice : vertices) {
             System.out.println(vertice.toString());   
         }
+
+        //Se imprime la matriz de adyacencia
         System.out.println("matriz de adyacencia");
-        
         for (int i = 0; i < vertices.size(); i++) {
-            
             for (int j = 0; j < vertices.size(); j++) {
                 System.out.print(adyacencia[i][j]);  
             }
